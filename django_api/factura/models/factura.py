c28ded6878c8194099181e5b07a0c1c220d06d15
JuanMajin <juan.majin@correounivalle.edu.co>
2022-12-19 00:16:19 -0500

    Se termina todas las migraciones de factura

--- django_api/factura/models/factura.py
+++ django_api/factura/models/factura.py
@@ -19,7 +19,47 @@ class Factura(DateBaseModel):
         help_text=(
             'Ayuda a saber si la factura esta activa o inactiva'
         )
+    
     )
+
+    fecha_expedicion= models.DateField(
+        'Fecha de expedcion'
+    )
+
+    fecha_vencimiento = models.DateField(
+        'Fecha de vencimiento'
+    )
+
+    total_pago = models.IntegerField(
+        'Total a pagar',
+        max_length= 20
+    )
+
+    valor_pendiente_pago = models.IntegerField(
+        'Valor pendiente de pago',
+        max_length=20
+    )
+
+    porcentaje_pago = models.BigIntegerField(
+        'Porcentaje de pago',
+        max_length=any
+    )
+    valor_recargo = models.IntegerField(
+        'Valor de recargo',
+        max_length= 20
+    )
+
+    recargo = models.BooleanField(
+        'Recargo',
+        default= False,
+        help_text=(
+            'ayuda a saber si se le aplica recargo o no'
+        )
+    )
+
+
+
+
     def __str__(self):
         return self.id  
 
