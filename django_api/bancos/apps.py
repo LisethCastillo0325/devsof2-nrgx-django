b901ae19b95ca884aa6774efecaca397dfcc25db
LisethCastillo <lcastillo@uni2.com.co>
2023-04-14 15:48:32 -0500

    Creación de modelo de Bancos

---
+++ django_api/bancos/apps.py
@@ -0,0 +1,9 @@
+"""Factura app."""
+
+# Django
+from django.apps import AppConfig
+
+class BancosAppConfig(AppConfig):
+    """Facturas app config."""
+    name = 'django_api.bancos'
+    verbose_name = 'Bancos'
---
+++ django_api/bancos/models/__init__.py
@@ -0,0 +1,1 @@
+from .bancos import Bancos
