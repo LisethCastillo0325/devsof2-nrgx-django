c4e2f79091c54baa8f18306ac85224cb40587d37
LisethCastillo <lcastillo@uni2.com.co>
2023-04-26 21:19:29 -0500

    pruebas basicas

--- django_api/test/test_user.py
+++ django_api/test/test_user.py
@@ -1,6 +1,5 @@
 import pytest
 
-from django.test import TestCase
 from faker import Faker
 
 from django_api.users.models.users import User
@@ -13,31 +12,14 @@ fake.add_provider(EmailProvider)
 from django_api.users.models.users import User
 
 
-# @pytest.mark.django_db
-# def test_super_user_create(user_creation):
-#     print('user:', user_creation.first_name)
-#     user_creation.is_superuser = True
-#     user_creation.save()
-#     assert user_creation.is_superuser
+@pytest.mark.django_db
+def test_super_user_create(user_creation):
+    user_creation.is_superuser = True
+    user_creation.save()
+    assert user_creation.is_superuser
 
-# def test_user_creaction_fail():
-#     with pytest.raises(Exception):
-#         User.objects.create(
-#              email='user@gmail.com'
-#         )
-
-
-class UsuarioTestCase(TestCase):
-
-    def setUp(self):
-        self.user = User.objects.create(
-            email=fake.custom_email(), 
-            password=fake.phone_number(),
-            identification_number='123456789',
-            first_name=fake.first_name(),
-            last_name=fake.last_name()
+def test_user_creaction_fail():
+    with pytest.raises(Exception):
+        User.objects.create(
+             email='user@gmail.com'
         )
-        return super().setUp()
-    
-    def test_user_creation(self):
-        self.assertEqual(self.user.is_superuser, False)
